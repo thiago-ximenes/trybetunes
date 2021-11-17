afd1952236d8912f499b8df76f32f08ada080122
Thiago Ximenes <thiagoximeneslima@gmail.com>
2021-11-17 19:47:40 -0300

    ✅ pass test: request 3 - show username at the screen with a loading tag while it is waiting for response.

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,41 @@
 import React, { Component } from 'react';
+import ReactLoading from 'react-loading';
 import { getUser } from '../services/userAPI';
 
 class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      userName: '',
+      isLoading: false,
+    };
+  }
+
+  componentDidMount() {
+    this.getUserName();
+  }
+
+  getUserName = async () => {
+    this.setState({ isLoading: true });
+    const response = await getUser();
+    this.setState({ isLoading: false });
+    return this.setState({ userName: response.name });
+  }
+
   render() {
+    const { userName, isLoading } = this.state;
     return (
       <div data-testid="page-search">
-        <h2 data-testid="header-user-name">{ getUser.name }</h2>
+        {
+          isLoading ? (
+            <>
+              <ReactLoading type="spokes" color="blue" height="20%" width="20%" />
+              <p>Carregando...</p>
+            </>
+          )
+            : <h2 data-testid="header-user-name">{ userName }</h2>
+
+        }
       </div>
     );
   }
