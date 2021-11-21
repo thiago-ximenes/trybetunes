deb9d19771d964bf8566d63e964b48272fdc15ea
Thiago Ximenes <thiagoximeneslima@gmail.com>
2021-11-21 16:01:31 -0300

    ✅ pass test: requirement 04.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,34 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
   render() {
     return (
       <header data-testid="header-component">
-        Cabeçalho
+        <Link
+          data-testid="link-to-search"
+          to={ {
+            pathname: '/search',
+          } }
+        >
+          Pesquisa
+        </Link>
+        <Link
+          data-testid="link-to-favorites"
+          to={ {
+            pathname: '/favorites',
+          } }
+        >
+          Favoritos
+        </Link>
+        <Link
+          data-testid="link-to-profile"
+          to={ {
+            pathname: '/profile',
+          } }
+        >
+          Perfil
+        </Link>
       </header>
     );
   }
