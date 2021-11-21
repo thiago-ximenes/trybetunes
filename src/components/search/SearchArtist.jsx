5567144526db334108345196c2dae58cf53cbacd
Thiago Ximenes <thiagoximeneslima@gmail.com>
2021-11-21 18:56:10 -0300

    💄 fix style: ajust style

--- src/components/search/SearchArtist.jsx
+++ src/components/search/SearchArtist.jsx
@@ -47,72 +47,76 @@ class SearchArtist extends Component {
       const {
         input, isDisabled, isRequisitionDone, artistAlbums, artistName } = this.state;
       return (
-        <Container>
+        <>
           { isRequisitionDone ? (
-            <Form>
-              <Row className="align-items-center">
-                <Col>
-                  <Form.Control
-                    value={ input }
-                    onChange={ handleChange }
-                    data-testid="search-artist-input"
-                    placeholder="nome do artista"
-                  />
-                </Col>
-                <Col xs="auto">
-                  <Button
-                    type="submit"
-                    onClick={ handleClick }
-                    disabled={ isDisabled }
-                    data-testid="search-artist-button"
-                  >
-                    Pesquisar
-                  </Button>
-                </Col>
-              </Row>
-            </Form>
+            <Container className="mb-3">
+              <Form>
+                <Row className="align-items-center">
+                  <Col>
+                    <Form.Control
+                      value={ input }
+                      onChange={ handleChange }
+                      data-testid="search-artist-input"
+                      placeholder="nome do artista"
+                    />
+                  </Col>
+                  <Col xs="auto">
+                    <Button
+                      type="submit"
+                      onClick={ handleClick }
+                      disabled={ isDisabled }
+                      data-testid="search-artist-button"
+                    >
+                      Pesquisar
+                    </Button>
+                  </Col>
+                </Row>
+              </Form>
+            </Container>
           ) : (
             <Loading />
           )}
-          {
-            artistAlbums.length !== 0
-              ? (
-                <div>
-                  <Alert variant="success">
-                    { `Resultado de álbuns de: ${artistName}` }
-                  </Alert>
-                  <Row>
-                    {
-                      artistAlbums.map((artist) => (
-                        <Col key={ artist.collectionId }>
-                          <Card style={ { width: '18rem' } }>
-                            <Card.Img variant="top" src={ artist.artworkUrl100 } />
-                            <Card.Body>
-                              <Card.Title>{ artist.collectionName }</Card.Title>
-                              <Card.Text>
-                                { artist.artistName }
-                              </Card.Text>
-                              <Button
-                                data-testid={ `link-to-album-${artist.collectionId}` }
-                                variant="primary"
-                                as={ Link }
-                                to={ {
-                                  pathname: `/album/${artist.collectionId}`,
-                                } }
-                              >
-                                Go somewhere
-                              </Button>
-                            </Card.Body>
-                          </Card>
-                        </Col>
-                      ))
-                    }
-                  </Row>
-                </div>
-              )
-              : <Alert variant="danger">Nenhum álbum foi encontrado</Alert>
-          }
-        </Container>
+          <Container fluid="sm">
+            {
+              artistAlbums.length !== 0
+                ? (
+                  <div>
+                    <Alert variant="success">
+                      { `Resultado de álbuns de: ${artistName}` }
+                    </Alert>
+                    <Row>
+                      {
+                        artistAlbums.map((artist) => (
+                          <Col key={ artist.collectionId }>
+                            <Card style={ { width: '18rem' } }>
+                              <Card.Img variant="top" src={ artist.artworkUrl100 } />
+                              <Card.Body>
+                                <Card.Title>{ artist.collectionName }</Card.Title>
+                                <Card.Text>
+                                  { artist.artistName }
+                                </Card.Text>
+                                <Button
+                                  data-testid={ `link-to-album-${artist.collectionId}` }
+                                  variant="primary"
+                                  as={ Link }
+                                  to={ {
+                                    pathname: `/album/${artist.collectionId}`,
+                                  } }
+                                >
+                                  Go somewhere
+                                </Button>
+                              </Card.Body>
+                            </Card>
+                          </Col>
+                        ))
+                      }
+                    </Row>
+                  </div>
+                )
+                : <Alert variant="danger">Nenhum álbum foi encontrado</Alert>
+            }
+          </Container>
+        </>
       );
     }
 }
