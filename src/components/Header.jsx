003eb7d578076d694e75b6b6d629e0b4345d4776
Thiago Ximenes <thiagoximeneslima@gmail.com>
2021-11-21 16:31:29 -0300

    💄 style component: requirement 04.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,34 +1,52 @@
 import React, { Component } from 'react';
+import { Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 class Header extends Component {
   render() {
     return (
       <header data-testid="header-component">
-        <Link
-          data-testid="link-to-search"
-          to={ {
-            pathname: '/search',
-          } }
+        <Navbar
+          collapseOnSelect
+          expand="lg"
+          bg="primary"
+          variant="dark"
+          className="justify-content-center"
         >
-          Pesquisa
-        </Link>
-        <Link
-          data-testid="link-to-favorites"
-          to={ {
-            pathname: '/favorites',
-          } }
-        >
-          Favoritos
-        </Link>
-        <Link
-          data-testid="link-to-profile"
-          to={ {
-            pathname: '/profile',
-          } }
-        >
-          Perfil
-        </Link>
+          <Nav justify variant="pills">
+            <Nav.Item>
+              <Nav.Link
+                data-testid="link-to-search"
+                as={ Link }
+                to={ {
+                  pathname: '/search',
+                } }
+              >
+                Pesquisa
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link
+                data-testid="link-to-favorites"
+                as={ Link }
+                to={ {
+                  pathname: '/favorites',
+                } }
+              >
+                Favoritos
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Link
+              as={ Link }
+              data-testid="link-to-profile"
+              to={ {
+                pathname: '/profile',
+              } }
+            >
+              Perfil
+            </Nav.Link>
+          </Nav>
+        </Navbar>
       </header>
     );
   }
