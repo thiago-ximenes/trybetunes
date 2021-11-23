1a20cc136caf487c3c5b39e82e6d6f825b95e566
Thiago Ximenes <thiagoximeneslima@gmail.com>
2021-11-23 09:25:47 -0300

    :rocket: : Até que enfim 80%.

--- src/components/Album.jsx
+++ src/components/Album.jsx
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 import { Container, Badge, Row, Col, Card } from 'react-bootstrap';
 import getMusics from '../services/musicsAPI';
 import MusicCard from './MusicCard';
+import { getFavoriteSongs } from '../services/favoriteSongsAPI';
+import Loading from './Loading';
 
 class Album extends Component {
   constructor() {
@@ -12,11 +14,22 @@ class Album extends Component {
       artistName: '',
       albumName: '',
       img: '',
+      isLoading: false,
+      favoriteList: [],
     };
   }
 
   componentDidMount() {
     this.getAlbumMusics();
+    this.loadFavoriteSongs();
+  }
+
+  loadFavoriteSongs = async () => {
+    this.setState({ isLoading: true }, async () => {
+      const response = await getFavoriteSongs();
+      this.setState({ isLoading: false,
+        favoriteList: response });
+    });
   }
 
   getAlbumMusics = async () => {
@@ -31,37 +44,40 @@ class Album extends Component {
   }
 
   render() {
-    const { albumMusics, artistName, albumName, img } = this.state;
+    const {
+      albumMusics, artistName, albumName, img, isLoading, favoriteList } = this.state;
     const { addSongToFavorite } = this;
     return (
-      <Container data-testid="page-album">
-        <Row className="justify-content-center">
-          <Col md="auto">
-            <h1>
-              <Badge data-testid="artist-name">
-                { artistName }
-              </Badge>
-            </h1>
-          </Col>
-        </Row>
-        <Row className="justify-content-center">
-          <Col md="auto">
-            <h2>
-              <Badge data-testid="album-name">
-                { albumName }
-              </Badge>
-            </h2>
-          </Col>
-        </Row>
-        <Row className="justify-content-center">
-          <Col md="auto">
-            <Card style={ { width: '18rem' } }>
-              <Card.Img src={ img } />
-            </Card>
-          </Col>
-          <Col md="auto">
-            <Card style={ { width: '18rem' } }>
-              {albumMusics.map((music, index) => index !== 0
+      isLoading ? (<Loading />)
+        : (
+          <Container data-testid="page-album">
+            <Row className="justify-content-center">
+              <Col md="auto">
+                <h1>
+                  <Badge data-testid="artist-name">
+                    { artistName }
+                  </Badge>
+                </h1>
+              </Col>
+            </Row>
+            <Row className="justify-content-center">
+              <Col md="auto">
+                <h2>
+                  <Badge data-testid="album-name">
+                    { albumName }
+                  </Badge>
+                </h2>
+              </Col>
+            </Row>
+            <Row className="justify-content-center">
+              <Col md="auto">
+                <Card style={ { width: '18rem' } }>
+                  <Card.Img src={ img } />
+                </Card>
+              </Col>
+              <Col md="auto">
+                <Card style={ { width: '18rem' } }>
+                  {albumMusics.map((music, index) => index !== 0
               && (
                 <MusicCard
                   favorite={ () => addSongToFavorite(albumMusics[index]) }
@@ -70,12 +86,14 @@ class Album extends Component {
                   trackName={ music.trackName }
                   trackId={ music.trackId }
                   music={ music }
+                  favoriteList={ favoriteList }
                 />
               ))}
-            </Card>
-          </Col>
-        </Row>
-      </Container>
+                </Card>
+              </Col>
+            </Row>
+          </Container>
+        )
     );
   }
 }
--- src/components/MusicCard.jsx
+++ src/components/MusicCard.jsx
@@ -13,6 +13,17 @@ class MusicCard extends Component {
     };
   }
 
+  componentDidMount() {
+    this.isChecked();
+  }
+
+  isChecked = () => {
+    const { favoriteList, music } = this.props;
+    this.setState({ checked: favoriteList.some((
+      song,
+    ) => song.trackId === music.trackId) });
+  }
+
   addSongToFavorite = async (trackSong) => {
     this.setState({ isLoading: true });
     await addSong(trackSong);
@@ -20,10 +31,13 @@ class MusicCard extends Component {
   }
 
   handleChange = ({ target: { checked } }) => {
+    const { music } = this.props;
     console.log(checked);
     if (checked) {
       this.setState({ checked: true });
-      return this.addSongToFavorite();
+      this.addSongToFavorite(music);
+    } else {
+      this.setState({ checked: false });
     }
   }
 
@@ -33,35 +47,37 @@ class MusicCard extends Component {
     const { handleChange } = this;
     return (
       isLoading ? (<Loading />)
-        : (<Card>
-          <Row>
-            <Card.Title variant="top">
-              { music.trackName }
-            </Card.Title>
-            <audio data-testid="audio-component"
-              src={ music.previewUrl }
-              controls
-            >
-              <track kind="captions" />
-              O seu navegador não suporta o elemento
-              {' '}
-              <code>audio</code>
-            </audio>
-          </Row>
-          <Row>
-            <Form>
-              <Col>
-                <Form.Check
-                  checked={ checked }
-                  onChange={ handleChange }
-                  type="checkbox"
-                  label="Favorita"
-                  data-testid={ `checkbox-music-${music.trackId}` }
-                />
-              </Col>
-            </Form>
-          </Row>
-        </Card>)
+        : (
+          <Card>
+            <Row>
+              <Card.Title variant="top">
+                { music.trackName }
+              </Card.Title>
+              <audio
+                data-testid="audio-component"
+                src={ music.previewUrl }
+                controls
+              >
+                <track kind="captions" />
+                O seu navegador não suporta o elemento
+                {' '}
+                <code>audio</code>
+              </audio>
+            </Row>
+            <Row>
+              <Form>
+                <Col>
+                  <Form.Check
+                    checked={ checked }
+                    onChange={ handleChange }
+                    type="checkbox"
+                    label="Favorita"
+                    data-testid={ `checkbox-music-${music.trackId}` }
+                  />
+                </Col>
+              </Form>
+            </Row>
+          </Card>)
     );
   }
 }
@@ -70,7 +86,8 @@ MusicCard.propTypes = {
   trackName: propTypes.string.isRequired,
   previewUrl: propTypes.string.isRequired,
   trackId: propTypes.string.isRequired,
-  favorite: propTypes.func.isRequired,
+  music: propTypes.shape.isRequired,
+  favoriteList: propTypes.arrayOf.isRequired,
 };
 
 export default MusicCard;
