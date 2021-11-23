1a82abc8f8eed5fba81070b4c81aea9295584cad
Thiago Ximenes <thiagoximeneslima@gmail.com>
2021-11-23 03:32:20 -0300

    ♻️ Refactory: requirement 8.

--- src/components/Album.jsx
+++ src/components/Album.jsx
@@ -2,9 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Container, Badge, Row, Col, Card } from 'react-bootstrap';
 import getMusics from '../services/musicsAPI';
-import { addSong } from '../services/favoriteSongsAPI';
 import MusicCard from './MusicCard';
-import Loading from './Loading';
 
 class Album extends Component {
   constructor() {
@@ -14,18 +12,11 @@ class Album extends Component {
       artistName: '',
       albumName: '',
       img: '',
-      isLoading: false,
     };
   }
 
   componentDidMount() {
     this.getAlbumMusics();
-  }
-
-  addSongToFavorite = async (trackSong) => {
-    this.setState({ isLoading: true });
-    await addSong(trackSong);
-    this.setState({ isLoading: false });
   }
 
   getAlbumMusics = async () => {
@@ -40,7 +31,7 @@ class Album extends Component {
   }
 
   render() {
-    const { albumMusics, artistName, albumName, img, isLoading } = this.state;
+    const { albumMusics, artistName, albumName, img } = this.state;
     const { addSongToFavorite } = this;
     return (
       <Container data-testid="page-album">
@@ -70,20 +61,16 @@ class Album extends Component {
           </Col>
           <Col md="auto">
             <Card style={ { width: '18rem' } }>
-              {
-                isLoading && <Loading />
-              }
               {albumMusics.map((music, index) => index !== 0
               && (
-                <div style={ { display: isLoading ? 'none' : 'block' } }>
-                  <MusicCard
-                    favorite={ () => addSongToFavorite(albumMusics[index]) }
-                    previewUrl={ music.previewUrl }
-                    key={ music.trackName }
-                    trackName={ music.trackName }
-                    trackId={ music.trackId }
-                  />
-                </div>
+                <MusicCard
+                  favorite={ () => addSongToFavorite(albumMusics[index]) }
+                  previewUrl={ music.previewUrl }
+                  key={ music.trackName }
+                  trackName={ music.trackName }
+                  trackId={ music.trackId }
+                  music={ music }
+                />
               ))}
             </Card>
           </Col>
--- src/components/MusicCard.jsx
+++ src/components/MusicCard.jsx
@@ -1,36 +1,67 @@
 import React, { Component } from 'react';
 import { Card, Row, Col, Form } from 'react-bootstrap';
 import propTypes from 'prop-types';
+import Loading from './Loading';
+import { addSong } from '../services/favoriteSongsAPI';
 
 class MusicCard extends Component {
+  constructor() {
+    super();
+    this.state = {
+      checked: false,
+      isLoading: false,
+    };
+  }
+
+  addSongToFavorite = async (trackSong) => {
+    this.setState({ isLoading: true });
+    await addSong(trackSong);
+    this.setState({ isLoading: false });
+  }
+
+  handleChange = ({ target: { checked } }) => {
+    console.log(checked);
+    if (checked) {
+      this.setState({ checked: true });
+      return this.addSongToFavorite();
+    }
+  }
+
   render() {
-    const { trackName, previewUrl, trackId, favorite } = this.props;
+    const { music } = this.props;
+    const { checked, isLoading } = this.state;
+    const { handleChange } = this;
     return (
-      <Card>
-        <Row>
-          <Card.Title variant="top">
-            { trackName }
-          </Card.Title>
-          <audio data-testid="audio-component" src={ previewUrl } controls>
-            <track kind="captions" />
-            O seu navegador não suporta o elemento
-            {' '}
-            <code>audio</code>
-          </audio>
-        </Row>
-        <Row>
-          <Form>
-            <Col>
-              <Form.Check
-                onChange={ favorite }
-                type="checkbox"
-                label="Favorita"
-                data-testid={ `checkbox-music-${trackId}` }
-              />
-            </Col>
-          </Form>
-        </Row>
-      </Card>
+      isLoading ? (<Loading />)
+        : (<Card>
+          <Row>
+            <Card.Title variant="top">
+              { music.trackName }
+            </Card.Title>
+            <audio data-testid="audio-component"
+              src={ music.previewUrl }
+              controls
+            >
+              <track kind="captions" />
+              O seu navegador não suporta o elemento
+              {' '}
+              <code>audio</code>
+            </audio>
+          </Row>
+          <Row>
+            <Form>
+              <Col>
+                <Form.Check
+                  checked={ checked }
+                  onChange={ handleChange }
+                  type="checkbox"
+                  label="Favorita"
+                  data-testid={ `checkbox-music-${music.trackId}` }
+                />
+              </Col>
+            </Form>
+          </Row>
+        </Card>)
     );
   }
 }
